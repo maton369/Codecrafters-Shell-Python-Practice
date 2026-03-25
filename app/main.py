38d62bd0336f9afc56494476ca7c5e75da4646d5
maton369 <ysk901085@gmail.com>
2026-03-25 10:44:46 +0900

    feat: type

--- app/main.py
+++ app/main.py
@@ -1,17 +1,33 @@
 import sys
 
+BUILTINS = {"echo", "exit", "type"}
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = input()
-
-        if command == "exit":
+        try:
+            command = input()
+        except EOFError:
             break
-        elif command.startswith("echo "):
-            print(command[5:])
+
+        parts = command.strip().split()
+
+        if len(parts) == 0:
+            continue
+
+        if parts[0] == "type":
+            if len(parts) < 2:
+                continue
+
+            target = parts[1]
+            if target in BUILTINS:
+                print(f"{target} is a shell builtin")
+            else:
+                print(f"{target} not found")
+
         else:
             print(f"{command}: command not found")
 
