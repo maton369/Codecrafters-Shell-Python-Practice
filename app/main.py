368756cdf4539a6362b1d9ff33a0f4617a31b268
maton369 <ysk901085@gmail.com>
2026-03-25 10:50:34 +0900

    feat: exit

--- app/main.py
+++ app/main.py
@@ -18,19 +18,18 @@ def main():
         if len(parts) == 0:
             continue
 
-        if parts[0] == "type":
+        if parts[0] == "exit":
+            break
+        elif parts[0] == "type":
             if len(parts) < 2:
                 continue
-
             target = parts[1]
             if target in BUILTINS:
                 print(f"{target} is a shell builtin")
             else:
                 print(f"{target} not found")
-
         elif parts[0] == "echo":
             print(" ".join(parts[1:]))
-
         else:
             print(f"{command}: command not found")
 
